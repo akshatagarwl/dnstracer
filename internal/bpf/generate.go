493a89469714a4e09fe30ac2c5596cb998785ac0
boratanrikulu <bora.tanrikulu@sematext.com>
2025-08-25 17:23:37 +0300

    remove ignore tag from generate.go

--- internal/bpf/generate.go
+++ internal/bpf/generate.go
@@ -1,5 +1,3 @@
-//go:build ignore
-
 package bpf
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go@latest -cc clang -cflags "-O2 -g -Wall -I../../bpf -I../../bpf/include" -type event_type -type trace_event_header -type dns_event -target amd64,arm64 Bpf ../../bpf/tracer.bpf.c
